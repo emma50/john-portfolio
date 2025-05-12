71373d8a75fafd11d61c69e9dd67b66c89043168
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2025-05-12 07:01:50 +0100

    WIP: Update

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <div className="w-full lg:w-2/3 flex flex-col items-center justify-center bg-blue-500">
+    <div className="w-full flex flex-col items-center justify-center bg-blue-500">
       <div className="invisible">This is an empty and invisible div</div>
-      <div className="flex flex-col items-center justify-center gap-6 p-4 bg-white shadow-md">
+      <div className="flex flex-col items-center justify-center gap-6 p-4 bg-white shadow-md w-full lg:w-2/3">
         <h1 className="text-4xl font-bold mb-4 text-center">Welcome to Our App</h1>
         <p className="text-xl font-bold text-red-400 uppercase">Free training reveals.</p>
       </div>
